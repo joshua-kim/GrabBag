97928e82dbe3123bc8974f2e886354ff7adbddc5
Josh Kim <kimjos@amazon.com>
2018-07-26 14:07:54 -0700

    fixed dependency issue

--- src/main/java/RetrieveRandomItemActivity.java
+++ src/main/java/RetrieveRandomItemActivity.java
@@ -1,19 +1,37 @@
-import org.omg.CORBA.NameValuePair;
+import org.apache.commons.httpclient.NameValuePair;
+import org.apache.commons.httpclient.methods.PostMethod;
 
+import java.io.IOException;
 import java.io.InputStream;
 
 public class RetrieveRandomItemActivity {
 
     public static void main(String[] args) {
 
-        PostMethod post = new PostMethod("http://jakarata.apache.org/");
+        PostMethod post = new PostMethod("mws.amazonservices.com");
+
+
         NameValuePair[] data = {
-                new NameValuePair("user", "joe"),
-                new NameValuePair("password", "bloggs")
+                new NameValuePair("AWSAccessKeyId", "AKIAEXAMPLEFWR4TJ7ZQ"),
+                new NameValuePair("Action", "ListMatchingProducts"),
+                new NameValuePair("MWSAuthToken", "amzn.mws.4ea38b7b-f563-7709-4bae-87aeaEXAMPLE"),
+                new NameValuePair("MarketplaceId", "ATVPDKIKX0DER"),
+                new NameValuePair("Query", "0439708184"),
+                new NameValuePair("SellerId", "A1IMEXAMPLEWRC"),
+                new NameValuePair("SignatureMethod", "HmacSHA256"),
+                new NameValuePair("SignatureVersion", "2"),
+                new NameValuePair("Timestamp", "2012-12-12T22%3A23%3A50Z"),
+                new NameValuePair("Version", "2011-10-01"),
+                new NameValuePair("Signature", "V%2BEXAMPLERT%2Baj%2Fxwqo7y3PIifMFHeqFlNYW0EXAMPLEA%3D")
         };
+
         post.setRequestBody(data);
-        // execute method and handle any error responses.
-        InputStream in = post.getResponseBodyAsStream();
-        // handle response.
+
+        try {
+            InputStream in = post.getResponseBodyAsStream();
+            System.out.println(post.getResponseBodyAsString());
+        } catch (IOException e){
+           e.printStackTrace();
+        }
     }
 }
